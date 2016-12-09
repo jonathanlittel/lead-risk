01b2680957938be0744554c87ff1a62e52574038
Jonathan Littel <rouefully@gmail.com>
2016-12-09 13:16:07 -0500

    update preprocessing

--- prep lead data.r
+++ prep lead data.r
@@ -8,18 +8,38 @@ setwd(wd)
 # ----------------------------------------------------------------
 # ----   read data -----------------------------------------------
 
+
+# rap_data <- read_csv('rap_data_Q4_15_05.24.16.csv')
+
+
+# Run script to load and process data
+  wd_mod <- paste0("C:/Box Sync/Risk Appetite - Provisioning Project/Working Folders for RAP Modules/",
+    "Risk Profile/PD Model/2.Model Selection", sep="")
+  setwd(wd_mod)
+  source('Preprocess data file.r') # Cleans and creates variables
+
 rap_data_dir <- paste0('C:/Box Sync/Risk Appetite - Provisioning Project/',
 	'Working Folders for RAP Modules/Risk Profile/PD Model/3.Outputs')
 setwd(rap_data_dir)
-rap_data <- read_csv('rap_data_Q4_15_05.24.16.csv')
 pd_data  <- read_csv('pds_08.19.16.csv')
 
-df <- pd_data %>%
+df.lead <- pd_data %>%
   select(RC.Opp.Number, pd, pd_one_year = pd_adjusted_pre_tenor) %>%
-  right_join(rap_data)
-
-setwd(wd)
-
-# prep data
+  right_join(df.rap)
 
 
+# ----------------------------------------------------------------
+# ----   prep data -----------------------------------------------
+
+# pre process
+
+df.lead <- preProcess(train)
+
+qualification_cutoff <- 0.10
+
+df.lead$preq_lgl <- df.lead$pd_one_year < qualification_cutoff
+df.lead$preq <- factor(as.numeric(df.lead$preq_lgl),
+	levels = 0:1,
+	labels = c('not_qualify', 'qualify'))
+
+prop.table(table(df.lead$preq_lgl))
